61b207b4a0efd443dd0d74d1a7d1c04fe8565994
kaser1997 <kaser.slaiby@gmail.com>
2024-01-08 13:41:43 +0300

    assets folder & DecorationImage

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -25,7 +25,10 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       height: 220,
       margin: EdgeInsets.only(left: 5, right: 5),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30), color: Color(0xFF69c5df)),
+          borderRadius: BorderRadius.circular(30),
+          color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage("assets/image/burger.png"))),
     );
   }
 }
