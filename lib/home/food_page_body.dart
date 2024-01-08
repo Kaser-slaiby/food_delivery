e843d96a45d46aa248c108a2c5d28be09497ebec
kaser1997 <kaser.slaiby@gmail.com>
2024-01-08 12:15:14 +0300

    add file dart for body

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({super.key});
+
+  @override
+  State<FoodPageBody> createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 320,
+      child: PageView.builder(
+          itemCount: 5,
+          itemBuilder: (context, position) {
+            return _buildPageItem(position);
+          }),
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    return Container(
+      height: 220,
+      margin: EdgeInsets.only(left: 5, right: 5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30), color: Color(0xFF69c5df)),
+    );
+  }
+}
--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/widgets/%20big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
+import 'food_page_body.dart';
+
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
 
@@ -13,46 +15,50 @@ class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            child: Container(
-              margin: EdgeInsets.only(top: 45, bottom: 15),
-              padding: EdgeInsets.only(left: 20, right: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      BigText(
-                        text: 'Syria',
-                      ),
-                      Row(
-                        children: [
-                          SmallText(
-                            text: 'Hama',
-                            color: Colors.black54,
-                          ),
-                          Icon(Icons.arrow_drop_down_rounded)
-                        ],
-                      )
-                    ],
-                  ),
-                  Container(
-                    width: 45,
-                    height: 45,
-                    child: Icon(Icons.search, color: Colors.white),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.cyanAccent,
+        body: Column(
+      children: [
+        Column(
+          children: [
+            Container(
+              child: Container(
+                margin: EdgeInsets.only(top: 45, bottom: 15),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        BigText(
+                          text: 'Syria',
+                        ),
+                        Row(
+                          children: [
+                            SmallText(
+                              text: 'Hama',
+                              color: Colors.black54,
+                            ),
+                            Icon(Icons.arrow_drop_down_rounded)
+                          ],
+                        )
+                      ],
                     ),
-                  )
-                ],
+                    Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.cyanAccent,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
-      ),
-    );
+            )
+          ],
+        ),
+        FoodPageBody(),
+      ],
+    ));
   }
 }
