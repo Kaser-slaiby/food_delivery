8ddf571e9bf7eaa120f37e9508bf3f29da1c1038
kaser1997 <kaser.slaiby@gmail.com>
2024-01-11 16:00:28 +0300

    add Matrix4

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -13,6 +13,8 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.90);
   var _currPageValue = 0.0;
+  double _scalFactor = 0.8;
+  double _height = 220;
 
   @override
   void initState() {
@@ -29,7 +31,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   Widget build(BuildContext context) {
     return Container(
       // color for test
-      color: Colors.amber,
+      // color: Colors.amber,
       height: 320,
       child: PageView.builder(
           controller: pageController,
@@ -47,91 +49,119 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currPageValue.floor()) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scalFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scalFactor + (_currPageValue - index + 1) * (1 - _scalFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() - 1) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scalFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scalFactor) / 2, 1);
+    }
+
     // Add Stake for height Container
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/image/pizza.jpeg"))),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: EdgeInsets.only(left: 30, right: 30, bottom: 25),
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30), color: Colors.white),
+                borderRadius: BorderRadius.circular(30),
+                color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/image/pizza.jpeg"))),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Container(
-              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Chinese Side"),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(5, (index) {
-                          return Icon(
-                            Icons.star,
-                            color: Colors.cyan,
-                            size: 15,
-                          );
-                        }),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      SmallText(text: "4.5"),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      SmallText(text: "1287"),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      SmallText(text: "comments"),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: [
-                      IconAndTextWidget(
-                          icon: Icons.circle_sharp,
-                          text: "Normal",
-                          iconColor: Colors.orangeAccent),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      IconAndTextWidget(
-                          icon: Icons.location_on,
-                          text: "1.7KM",
-                          iconColor: Colors.cyanAccent),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      IconAndTextWidget(
-                          icon: Icons.access_time_rounded,
-                          text: "32min",
-                          iconColor: Colors.deepOrangeAccent),
-                    ],
-                  )
-                ],
+              height: 120,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 25),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.white),
+              child: Container(
+                padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Chinese Side"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(
+                              Icons.star,
+                              color: Colors.cyan,
+                              size: 15,
+                            );
+                          }),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "4.5"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "1287"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "comments"),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        IconAndTextWidget(
+                            icon: Icons.circle_sharp,
+                            text: "Normal",
+                            iconColor: Colors.orangeAccent),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        IconAndTextWidget(
+                            icon: Icons.location_on,
+                            text: "1.7KM",
+                            iconColor: Colors.cyanAccent),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        IconAndTextWidget(
+                            icon: Icons.access_time_rounded,
+                            text: "32min",
+                            iconColor: Colors.deepOrangeAccent),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
