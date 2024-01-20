4e0986fccccbbfbab53b2acae4a6c6406b7a1a84
kser-slaiby <kaser.slaiby@gmail.com>
2024-01-20 06:25:02 +0300

    add class file AppConstants for url

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -3,9 +3,11 @@ import 'package:food_delivery/data/api/api_client.dart';
 import 'package:food_delivery/data/repository/popular_product_repo.dart';
 import 'package:get/get.dart';
 
+import '../utils/app_constants.dart';
+
 Future<void> init() async {
   //api client
-  Get.lazyPut(() => ApiClient(appBaseUrl: "https://mvs.bslmeiyu.com"));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
 
   //repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
