94ec16fac08267297008c53c535735aaa189cf44
kaser1997 <kaser.slaiby@gmail.com>
2024-02-08 13:05:51 +0300

    Reset time of items during checkout

--- lib/data/repository/cart_repo.dart
+++ lib/data/repository/cart_repo.dart
@@ -14,16 +14,20 @@ class CartRepo {
   List<String> cartHistory = [];
 
   void addToCartList(List<CartModel> cartList) {
-    sharedPreferences.remove(AppConstants.CART_LIST);
-    sharedPreferences.remove(AppConstants.CART_HISTORY_LIST);
+    // sharedPreferences.remove(AppConstants.CART_LIST);
+    // sharedPreferences.remove(AppConstants.CART_HISTORY_LIST);
+    var time = DateTime.now().toString();
     cart = [];
     // cartList.forEach((element) {
     //   return cart.add(jsonEncode(element));
     // });
-    cartList.forEach((element) => cart.add(jsonEncode(element)));
+    cartList.forEach((element) {
+      element.time = time;
+      return cart.add(jsonEncode(element));
+    });
     sharedPreferences.setStringList(AppConstants.CART_LIST, cart);
     // print(sharedPreferences.getStringList(AppConstants.CART_LIST));
-    getCartList();
+    // getCartList();
   }
 
   List<CartModel> getCartList() {
