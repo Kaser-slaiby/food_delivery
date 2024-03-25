d8f298c7fc7a7e442f4b94a412e5d5958a6dbda0
Your Name <kaser.slaiby@gmail.com>
2024-03-25 20:45:16 +0300

    modified style

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -33,71 +33,105 @@ class AccountPage extends StatelessWidget {
               iconColor: Colors.white,
             ),
             SizedBox(height: Dimensions.height30),
-            AccountWidgets(
-              bigText: BigText(
-                text: "Kaser",
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    AccountWidgets(
+                      bigText: BigText(
+                        text: "Kaser",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.person,
+                        backgroundColor: Colors.orangeAccent,
+                        size: Dimensions.height10 * 5,
+                        iconSize: Dimensions.height10 * 5 / 2,
+                        iconColor: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: Dimensions.height15),
+                    AccountWidgets(
+                      bigText: BigText(
+                        text: "00000000",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.phone,
+                        backgroundColor: Colors.greenAccent,
+                        size: Dimensions.height10 * 5,
+                        iconSize: Dimensions.height10 * 5 / 2,
+                        iconColor: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: Dimensions.height15),
+                    AccountWidgets(
+                      bigText: BigText(
+                        text: "www@gmail.com",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.email,
+                        backgroundColor: Colors.greenAccent,
+                        size: Dimensions.height10 * 5,
+                        iconSize: Dimensions.height10 * 5 / 2,
+                        iconColor: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: Dimensions.height15),
+                    AccountWidgets(
+                      bigText: BigText(
+                        text: "Fill in your address",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.location_on,
+                        backgroundColor: Colors.orangeAccent,
+                        size: Dimensions.height10 * 5,
+                        iconSize: Dimensions.height10 * 5 / 2,
+                        iconColor: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: Dimensions.height15),
+                    AccountWidgets(
+                      bigText: BigText(
+                        text: "Message",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.message_outlined,
+                        backgroundColor: Colors.orangeAccent,
+                        size: Dimensions.height10 * 5,
+                        iconSize: Dimensions.height10 * 5 / 2,
+                        iconColor: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: Dimensions.height15),
+                    AccountWidgets(
+                      bigText: BigText(
+                        text: "Message",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.message_outlined,
+                        backgroundColor: Colors.orangeAccent,
+                        size: Dimensions.height10 * 5,
+                        iconSize: Dimensions.height10 * 5 / 2,
+                        iconColor: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: Dimensions.height15),
+                    AccountWidgets(
+                      bigText: BigText(
+                        text: "Message",
+                      ),
+                      appIcon: AppIcon(
+                        icon: Icons.message_outlined,
+                        backgroundColor: Colors.orangeAccent,
+                        size: Dimensions.height10 * 5,
+                        iconSize: Dimensions.height10 * 5 / 2,
+                        iconColor: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: Dimensions.height15),
+                  ],
+                ),
               ),
-              appIcon: AppIcon(
-                icon: Icons.person,
-                backgroundColor: Colors.orangeAccent,
-                size: Dimensions.height10 * 5,
-                iconSize: Dimensions.height10 * 5 / 2,
-                iconColor: Colors.white,
-              ),
-            ),
-            SizedBox(height: Dimensions.height15),
-            AccountWidgets(
-              bigText: BigText(
-                text: "00000000",
-              ),
-              appIcon: AppIcon(
-                icon: Icons.phone,
-                backgroundColor: Colors.greenAccent,
-                size: Dimensions.height10 * 5,
-                iconSize: Dimensions.height10 * 5 / 2,
-                iconColor: Colors.white,
-              ),
-            ),
-            SizedBox(height: Dimensions.height15),
-            AccountWidgets(
-              bigText: BigText(
-                text: "www@gmail.com",
-              ),
-              appIcon: AppIcon(
-                icon: Icons.email,
-                backgroundColor: Colors.greenAccent,
-                size: Dimensions.height10 * 5,
-                iconSize: Dimensions.height10 * 5 / 2,
-                iconColor: Colors.white,
-              ),
-            ),
-            SizedBox(height: Dimensions.height15),
-            AccountWidgets(
-              bigText: BigText(
-                text: "Fill in your address",
-              ),
-              appIcon: AppIcon(
-                icon: Icons.location_on,
-                backgroundColor: Colors.orangeAccent,
-                size: Dimensions.height10 * 5,
-                iconSize: Dimensions.height10 * 5 / 2,
-                iconColor: Colors.white,
-              ),
-            ),
-            SizedBox(height: Dimensions.height15),
-            AccountWidgets(
-              bigText: BigText(
-                text: "Message",
-              ),
-              appIcon: AppIcon(
-                icon: Icons.message_outlined,
-                backgroundColor: Colors.orangeAccent,
-                size: Dimensions.height10 * 5,
-                iconSize: Dimensions.height10 * 5 / 2,
-                iconColor: Colors.white,
-              ),
-            ),
-            SizedBox(height: Dimensions.height15),
+            )
           ],
         ),
       ),
