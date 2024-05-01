6ab378b2fb9267104fb86a64d2fcf324bee22b8f
Your Name <kaser.slaiby@gmail.com>
2024-05-01 10:09:00 +0300

    modified style splash page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:food_delivery/controllers/cart_controller.dart';
 import 'package:food_delivery/controllers/popular_product_controller.dart';
 import 'package:food_delivery/controllers/recommended_product_controller.dart';
+import 'package:food_delivery/pages/splash/splash_page.dart';
 import 'package:food_delivery/routes/route_helper.dart';
 import 'package:get/get.dart';
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
             return GetMaterialApp(
               debugShowCheckedModeBanner: false,
               title: 'Flutter Demo',
-              // home: SignInPage(),
+              // home:SplashScreen() ,
               initialRoute: RouteHelper.getSplashPage(),
               getPages: RouteHelper.routes,
             );
--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/routes/route_helper.dart';
 import 'package:food_delivery/utils/dimensions.dart';
@@ -32,34 +33,63 @@ class _SplashScreenState extends State<SplashScreen>
     controller =
         AnimationController(vsync: this, duration: const Duration(seconds: 2))
           ..forward();
-    animation = CurvedAnimation(parent: controller, curve: Curves.linear);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeInExpo);
 
-    Timer(Duration(seconds: 3), () => Get.offNamed(RouteHelper.getInitial()));
+    Timer(Duration(seconds: 4), () => Get.offNamed(RouteHelper.getInitial()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ScaleTransition(
-            scale: animation,
-            child: Center(
-              child: Image.asset(
-                "assets/image/logo2.png",
-                width: Dimensions.splashImg,
+      body: Container(
+        color: Colors.grey[200],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FittedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: ScaleTransition(
+                      scale: animation,
+                      child: Center(
+                        child: Image.asset(
+                          "assets/image/logo2.png",
+                          width: Dimensions.splashImg,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Image.asset(
+                        "assets/image/logo1.png",
+                        width: Dimensions.splashImg,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          Center(
-            child: Image.asset(
-              "assets/image/logo1.png",
-              width: Dimensions.splashImg,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: ScaleTransition(
+                    scale: animation,
+                    child: Image.asset(
+                      fit: BoxFit.contain,
+                      "assets/image/logo3.png",
+                      // width: Dimensions.splashImg,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
