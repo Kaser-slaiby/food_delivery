b3dc0f0782345e2ec9e25c38d15f2b194db98866
Your Name <kaser.slaiby@gmail.com>
2024-05-01 12:05:01 +0300

    change neam and icon app

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
             return GetMaterialApp(
               debugShowCheckedModeBanner: false,
               title: 'Flutter Demo',
-              // home:SplashScreen() ,
+              // home: SplashScreen(),
               initialRoute: RouteHelper.getSplashPage(),
               getPages: RouteHelper.routes,
             );
