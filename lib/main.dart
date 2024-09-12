2fac4c9225bf8173f9aa5fb53dc18946727b1408
Your Name <kaser.slaiby@gmail.com>
2024-09-12 14:57:06 +0300

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application..
   @override
   Widget build(BuildContext context) {
     Get.find<CartController>().getCartData();
